b18e8c90e6b8d2682bcc07cff82f2887678616d1
Alexander Belov <marlo-nn@yandex.ru>
2023-02-14 10:41:24 +0300

    feat: template

--- scripts/index.js
+++ scripts/index.js
@@ -10,22 +10,19 @@ const initialItems = [
 
 const itemListWrapper = document.querySelector('.items');
 const form = document.querySelector('.add_form');
+const template = document.getElementById('item')
+
 
 // функция которая создает элементы
+const getItemElement = (title) => {
+  const newItemElement = template.content.cloneNode(true);
+  const newItemTitle = newItemElement.querySelector('.todo_item__header');
+  newItemTitle.textContent = title;
+  return newItemElement;
+};
+
 const renderItem = (wrap, title) => {
-  wrap.insertAdjacentHTML('beforebegin', `
-
-  <li class="todo_item">
-  <h3 class="todo_item__header">${title}</h3>
-  <div class="buttons">
-    <button type="button" class="button button__edit"></button>
-    <button type="button" class="button button__duplicate"></button>
-    <button type="button" class="button button__delete"></button>
-  </div>
-</li>
-
-`);
-
+  wrap.append(getItemElement(title));
 };
 
 initialItems.forEach((title) => {
@@ -37,6 +34,8 @@ form.addEventListener('submit', (evt) => {
   evt.preventDefault();
   const title = evt.target.title.value;
   console.log(`при нажатии на сабмит : ${title}`);
-  renderItem(itemListWrapper, title);
-  evt.target.title.value = '';
+  if (title) {
+    renderItem(itemListWrapper, title);
+    evt.target.title.value = '';
+  };
 });
